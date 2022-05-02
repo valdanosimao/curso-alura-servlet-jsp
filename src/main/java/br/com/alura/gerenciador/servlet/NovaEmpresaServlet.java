d305a21c7216a0a8925ca580f5b6c9013fa837a9
Valdano Simão <valdanosimao@gmail.com>
2022-05-02 17:05:45 -0300

    redirecionamento de fluxo

--- src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
+++ src/main/java/br/com/alura/gerenciador/servlet/NovaEmpresaServlet.java
@@ -40,9 +40,13 @@ public class NovaEmpresaServlet extends HttpServlet {
 		Banco banco = new Banco();		
 		banco.adiciona(empresa);	
 		
-		//chamar o JSP
-		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
 		request.setAttribute("empresa", empresa.getNome());
-		rd.forward(request, response);
+		
+		response.sendRedirect("listaEmpresas");
+		
+//		//chamar o JSP
+//		RequestDispatcher rd = request.getRequestDispatcher("/novaEmpresaCriada.jsp");
+//		request.setAttribute("empresa", empresa.getNome());
+//		rd.forward(request, response);
 }
 }
